600da53050d4e7b80d278954e1a9f7320e591bb5
Anass Baba <anass.baba@insa-lyon.fr>
2024-07-15 08:26:26 +0200

    add: script and notebook

---
+++ src/script/dashboard_final.py
@@ -0,0 +1,109 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Titre de l'application
+st.title('Élèves en difficulté à prioriser ')
+
+# Interface pour télécharger le fichier CSV
+uploaded_file = st.file_uploader("Téléchargez un fichier CSV", type=['csv'])
+
+if uploaded_file is not None:
+    # Lecture du fichier CSV
+    df = pd.read_csv(uploaded_file, encoding='ISO-8859-1')
+
+    # Affichage des premières lignes du DataFrame pour vérification
+    st.write("Aperçu des premières lignes du fichier téléchargé :")
+    st.write(df.head())
+
+    # Liste des colonnes attendues dans le fichier CSV
+    colonnes_attendues = ['StudentID', 'FamilyName', 'FirstName', 'sex', 'age', 'address', 'famsize', 'Pstatus', 
+                          'Medu', 'Fedu', 'Mjob', 'Fjob', 'reason', 'guardian', 
+                          'traveltime', 'studytime', 'failures', 'schoolsup', 
+                          'famsup', 'paid', 'activities', 'nursery', 'higher', 
+                          'internet', 'romantic', 'famrel', 'freetime', 'goout', 
+                          'Dalc', 'Walc', 'health', 'absences', 'FinalGrade']
+
+    # Vérification de la présence des colonnes attendues
+    colonnes_manquantes = [colonne for colonne in colonnes_attendues if colonne not in df.columns]
+
+    if len(colonnes_manquantes) == 0:
+        st.success("Toutes les colonnes requises sont présentes dans le fichier CSV.")
+
+
+
+        # Normalisation pour certains paramètres
+        df['failures'] = (df['failures'] - df['failures'].mean()) / df['failures'].std()
+        df['goout'] = (df['goout'] - df['goout'].mean()) / df['goout'].std()
+        df['Dalc'] = (df['Dalc'] - df['Dalc'].mean()) / df['Dalc'].std()
+        df['Walc'] = (df['Walc'] - df['Walc'].mean()) / df['Walc'].std()
+        df['absences'] = (df['absences'] - df['absences'].mean()) / df['absences'].std()
+        df['studytime'] = (df['studytime'] - df['studytime'].mean()) / df['studytime'].std()
+
+        # Inverser les paramètres péjoratifs en les soustrayant de la valeur maximale
+        df['inverse_failures'] = df['failures'].max() - df['failures']
+        df['inverse_goout'] = df['goout'].max() - df['goout']
+        df['inverse_Dalc'] = df['Dalc'].max() - df['Dalc']
+        df['inverse_Walc'] = df['Walc'].max() - df['Walc']
+        df['inverse_absences'] = df['absences'].max() - df['absences']
+        
+        # Calcul de l'Improvement Score 
+        df['Improvement Score'] = 0.3 * df['inverse_failures'] + 0.19 * df['inverse_goout'] + 0.15 * df['inverse_Dalc'] +0.19 * df['inverse_Walc'] + 0.24 * df['inverse_absences'] + 0.11 * df['studytime']
+
+        # Définir le seuil pour la note finale en mathématiques
+        seuil_note = 10
+
+        # Filtrage des étudiants avec une note finale basse mais un bon Improvement Score
+        students_to_highlight = df[(df['FinalGrade'] < seuil_note) ]
+
+        # Création du Scatter Plot interactif avec Plotly Express pour tous les élèves
+        fig_all_students = px.scatter(df, x='FinalGrade', y='Improvement Score', color='age',
+                                      hover_data=['age', 'studytime', 'failures', 'absences'],
+                                      labels={'FinalGrade': 'Note finale en mathématiques', 'Improvement Score': 'Improvement Score'})
+
+        # Personnalisation du layout du graphique pour tous les élèves
+        fig_all_students.update_layout(title='Tous les élèves',
+                                       xaxis_title='Note finale en mathématiques',
+                                       yaxis_title='Improvement Score')
+        # Mise à jour des traces pour rendre les points cliquables
+        fig_all_students.update_traces(mode='markers', marker={'size': 10},
+                                               customdata=df[['StudentID', 'FamilyName', 'FirstName']].values,
+                                               hovertemplate='<b>Student ID:</b> %{customdata[0]}<br>'
+                                                             '<b>FamilyName:</b> %{customdata[1]}<br>'
+                                                             '<b>FirstName:</b> %{customdata[2]}<br>')
+
+        # Création du Scatter Plot interactif avec Plotly Express pour les élèves à mettre en évidence
+        fig_highlighted_students = px.scatter(students_to_highlight, x='FinalGrade', y='Improvement Score', color='age',
+                                              hover_data=['age', 'studytime', 'failures', 'absences'],
+                                              labels={'FinalGrade': 'Note finale en mathématiques', 'Improvement Score': 'Improvement Score'})
+
+        # Personnalisation du layout du graphique pour les élèves à mettre en évidence
+        fig_highlighted_students.update_layout(title='Zoom sur les élèves avec une note en mathématiques en dessous de la moyenne',
+                                               xaxis_title='Note finale en mathématiques',
+                                               yaxis_title='Improvement Score')
+
+        # Mise à jour des traces pour rendre les points cliquables
+        fig_highlighted_students.update_traces(mode='markers', marker={'size': 10},
+                                               customdata=students_to_highlight[['StudentID', 'FamilyName', 'FirstName']].values,
+                                               hovertemplate='<b>Student ID:</b> %{customdata[0]}<br>'
+                                                             '<b>FamilyName:</b> %{customdata[1]}<br>'
+                                                             '<b>FirstName:</b> %{customdata[2]}<br>')
+
+        # Affichage du Scatter Plot interactif pour tous les élèves
+        st.plotly_chart(fig_all_students)
+
+        # Affichage du Scatter Plot interactif pour les élèves à mettre en évidence
+        st.plotly_chart(fig_highlighted_students)
+
+        # Affichage du classement des élèves à prendre en charge
+        st.subheader('Les élèves en difficultés à prendre en charge :')
+        # Trier les étudiants à mettre en évidence d'abord par FinalGrade, puis par Improvement Score
+        students_sorted = students_to_highlight.sort_values(by=['FinalGrade', 'Improvement Score'], ascending=[True, False])
+        # Affichage du classement des élèves à prendre en charge
+        st.write(students_sorted[['StudentID', 'FamilyName', 'FirstName', 'Improvement Score', 'FinalGrade']])
+
+        
+    else:
+        st.error(f"Les colonnes suivantes sont manquantes : {', '.join(colonnes_manquantes)}")
+else:
+    st.info("Veuillez télécharger un fichier CSV pour commencer l'analyse.")
